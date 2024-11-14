4910d5688fc6c0e41576391795b29b3600c058df
sug4chy <sb.9226981433@gmail.com>
2024-11-14 20:45:39 +0500

    feat: добавил данные для чётной недели

--- app/src/main/java/ru/sug4chy/timetable/utils/TimetableData.kt
+++ app/src/main/java/ru/sug4chy/timetable/utils/TimetableData.kt
@@ -140,7 +140,141 @@ object TimetableData {
                 )
             ),
             2 to mapOf(
-
+                DayOfWeek.MONDAY to TimetableDay(
+                    formatDayOfWeek(DayOfWeek.MONDAY),
+                    listOf(
+                        TimetableItem(
+                            lessonName = "Анализ данных (лек.)",
+                            startTime = "16.40",
+                            endTime = "18.10",
+                            classroomName = "А-17",
+                            teacherFullName = "Мирасов"
+                        ),
+                        TimetableItem(
+                            lessonName = "Базы и хранилища данных (лек.)",
+                            startTime = "18.20",
+                            endTime = "19.50",
+                            classroomName = "А-17",
+                            teacherFullName = "Барабанщиков"
+                        ),
+                    )
+                ),
+                DayOfWeek.TUESDAY to TimetableDay(
+                    formatDayOfWeek(DayOfWeek.TUESDAY),
+                    listOf(
+                        TimetableItem(
+                            lessonName = "Управление IT-проектами (лек.)",
+                            startTime = "18.20",
+                            endTime = "19.50",
+                            classroomName = "А-21",
+                            teacherFullName = "Воробьёв, Пущев"
+                        ),
+                        TimetableItem(
+                            lessonName = "Управление IT-проектами (пр.)",
+                            startTime = "19.55",
+                            endTime = "21.25",
+                            classroomName = "А-21",
+                            teacherFullName = "Воробьёв, Пущев"
+                        ),
+                    )
+                ),
+                DayOfWeek.WEDNESDAY to TimetableDay(
+                    formatDayOfWeek(DayOfWeek.WEDNESDAY),
+                    listOf(
+                        TimetableItem(
+                            lessonName = "Базы и хранилища данных (пр.)",
+                            startTime = "18.20",
+                            endTime = "19.50",
+                            classroomName = "132",
+                            teacherFullName = "Барабанщиков"
+                        ),
+                        TimetableItem(
+                            lessonName = "Базы и хранилища данных (пр.)",
+                            startTime = "19.55",
+                            endTime = "21.25",
+                            classroomName = "132",
+                            teacherFullName = "Барабанщиков"
+                        ),
+                    )
+                ),
+                DayOfWeek.THURSDAY to TimetableDay(
+                    formatDayOfWeek(DayOfWeek.THURSDAY),
+                    listOf(
+                        TimetableItem(
+                            lessonName = "Разработка интернет-приложений (лек.)",
+                            startTime = "19.30",
+                            endTime = "19.50",
+                            classroomName = "132",
+                            teacherFullName = "Павличенко"
+                        ),
+                        TimetableItem(
+                            lessonName = "Разработка интернет-приложений (пр.)",
+                            startTime = "19.55",
+                            endTime = "21.25",
+                            classroomName = "132",
+                            teacherFullName = "Павличенко"
+                        ),
+                    )
+                ),
+                DayOfWeek.FRIDAY to TimetableDay(
+                    formatDayOfWeek(DayOfWeek.FRIDAY),
+                    listOf(
+                        TimetableItem(
+                            lessonName = "Анализ данных (пр.)",
+                            startTime = "15.00",
+                            endTime = "16.30",
+                            classroomName = "132",
+                            teacherFullName = "Мирасов"
+                        ),
+                        TimetableItem(
+                            lessonName = "Анализ данных (пр.)",
+                            startTime = "16.30",
+                            endTime = "18.10",
+                            classroomName = "132",
+                            teacherFullName = "Мирасов"
+                        ),
+                        TimetableItem(
+                            lessonName = "Тестирование ПО, 2 - 10 нед. (лек.)",
+                            startTime = "19.30",
+                            endTime = "21.00",
+                            classroomName = "Дистант",
+                            teacherFullName = "Булавин"
+                        ),
+                    )
+                ),
+                DayOfWeek.SATURDAY to TimetableDay(
+                    formatDayOfWeek(DayOfWeek.SATURDAY),
+                    listOf(
+                        TimetableItem(
+                            lessonName = "Тестирование ПО, 1 - 9 нед. (пр.)",
+                            startTime = "11.20",
+                            endTime = "12.50",
+                            classroomName = "132",
+                            teacherFullName = "Булавин"
+                        ),
+                        TimetableItem(
+                            lessonName = "Тестирование ПО, 1 - 9 нед. (пр.)",
+                            startTime = "13.15",
+                            endTime = "14.45",
+                            classroomName = "132",
+                            teacherFullName = "Булавин"
+                        ),
+                        TimetableItem(
+                            lessonName = "ТПП (лек.)",
+                            startTime = "15.00",
+                            endTime = "16.30",
+                            classroomName = "326",
+                            teacherFullName = "Веригин"
+                        ),
+                        TimetableItem(
+                            lessonName = "ТПП (пр.)",
+                            startTime = "16.40",
+                            endTime = "18.10",
+                            classroomName = "326",
+                            teacherFullName = "Веригин"
+                        ),
+                    )
+                )
             )
         )
 
